2b6d191e3f597de2cfc28118dcd5dc9b9153fea6
Azlz <36884381+Azlz@users.noreply.github.com>
2018-11-13 15:51:32 +0100

    Partie1-V4
    avancement

--- Partie1/Partie1/Examen_IA.cs
+++ Partie1/Partie1/Examen_IA.cs
@@ -23,5 +23,19 @@ namespace Partie1bis
 
                 Console.Write("Question n°" + i + "/20"); 
         }
+
+        private void Examen_IA_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void validerRep_Click(object sender, EventArgs e)
+        {
+            List<string> rep = new List<string>();
+            rep.Add("1"); rep.Add("2"); rep.Add("3");
+            Question question = new Question("test", 2, rep);
+
+            affichageQuestion.Items.Add(question.ToString());
+        }
     }
 }
